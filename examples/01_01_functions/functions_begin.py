33d33ecd83185410180d4222dc127f3661ce4f1d
studmoltenimar-3a25 <stud.moltenimar@gdromagnosi.edu.it>
2026-04-09 09:46:53 +0000

    es1_modifiche

--- examples/01_01_functions/functions_begin.py
+++ examples/01_01_functions/functions_begin.py
@@ -1,8 +1,31 @@
 """ A Functional Breakfast """
+def mix_and_cook():
+    print('Mixing the ingredients')
+    print('Greasing the frying pan')
+    print('Pouring the mixture into a frying pan')
+    print('Cooking the first side')
+    print('Flipping it!')
+    print('Cooking the other side\n')
 
-print('Mixing the ingredients')
-print('Pouring the mixture into a frying pan')
-print('Cooking the first side')
-print('Flipping it!')
-print('Cooking the other side\n')
-omelette = 'a tasty omelette'
+def make_omelette(ingredients):
+    mix_and_cook()
+    omelette = f'a {ingredients} omelette'
+    return omelette
+
+def make_fancy_omelette(*ingredients):
+    mix_and_cook()
+    omelette = f' a fancy omelette with {len(ingredients)} ingredients'
+    return omelette
+
+def make_pancake():
+    mix_and_cook()
+    pancake = 'a delicious pancake'
+    return pancake
+
+
+# make breakfast for two
+Marco_breakfast = make_omelette('Bacon')
+Ronaldo_breakfast = make_fancy_omelette('Cheese', 'tomato', 'onion', 'mushroom', 'ham')
+print(f"Marco is having {Marco_breakfast}\n")
+print(f"Ronaldo is having {Ronaldo_breakfast}\n")
+
